1fdf24da86956a3c66a50b2d1bb398e7b88717b1
mabdulhadisrk <abdulhadisohailrk@gmail.com>
2026-07-14 23:52:25 +0500

    Auto-commit: 23:52

--- main.js
+++ main.js
@@ -1040,15 +1040,12 @@ function animate() {
     
     // Idle breathing - smoother
     player1.position.y = 0.1 + Math.sin(Date.now() * 0.003) * 0.05;
-    player1.rotation.y = Math.sin(Date.now() * 0.002) * 0.02;
     player2.position.y = 0.1 + Math.cos(Date.now() * 0.003) * 0.05;
-    player2.rotation.y = Math.cos(Date.now() * 0.002) * 0.02;
 
     //arm away
     p1Parts.rightArmGroup.rotation.z = -0.4 + Math.sin(Date.now() * 0.003) * 0.05;
     p1Parts.leftArmGroup.rotation.z = 0.4 + Math.cos(Date.now() * 0.003) * 0.05;
-    p2Parts.rightArmGroup.rotation.z = -0.4 + Math.sin(Date.now() * 0.003) * 0.05;
-    p2Parts.leftArmGroup.rotation.z = 0.4 + Math.cos(Date.now() * 0.003) * 0.05;
+    
     
     // Atmosphere particles
     atmosphereParticles.children.forEach((p, i) => {
